22632a743af51b8f3e3b20d787943e982d70a0bc
Matt <matt.hd.ford@gmail.com>
2020-11-09 21:24:33 +0000

    Add express_server, set up the server

---
+++ express_server.js
@@ -0,0 +1,23 @@
+// express_server.js
+
+const { request } = require("express");
+
+// Setting up a server
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (re, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
