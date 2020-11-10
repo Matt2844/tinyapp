d8c1be912fb83f4cef5632113f3debef1c3edada
Matt <matt.hd.ford@gmail.com>
2020-11-10 12:44:54 +0000

    tried to get the short url generator to work

--- express_server.js
+++ express_server.js
@@ -8,12 +8,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const generateRandomString = () => {
+  Math.random().toString(36).substring(6);
+};
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -36,7 +43,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-})
+});
+
+
